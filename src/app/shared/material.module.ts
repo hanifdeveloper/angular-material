57bd14d62837437da7a2b58b577fea3343a9d915
zakuan <mzackwan90@gmail.com>
2018-09-16 20:08:10 +0700

    firt comit

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
+  declarations: []
+})
+export class MaterialModule { }
